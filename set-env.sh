d561f057f2d1a246c803334e1786ccf4ecc0a97c
davidp1404 <58428863+davidp1404@users.noreply.github.com>
2022-02-20 09:19:38 +0100

    Update set-env.sh

--- set-env.sh
+++ set-env.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
+curl -LO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx /tmp
+install -ma+x /tmp/kubectx /usr/local/bin/kubectx
+curl -LO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens /tmp
+install -ma+x /tmp/kubecns /usr/local/bin/kubens
 echo "source <(kubectl completion bash)" >> ~/.bashrc 
 echo "source <(kubectl completion bash)" >> ~/.bashrc 
 echo "alias k=kubectl" >> ~/.bashrc
 echo "complete -F __start_kubectl k" >> ~/.bashrc
-source ~/.bashrc
+echo "Execute this: source ~/.bashrc"
