bb7bfdcfd7a5ef989927e59f1602b45c2f8b9455
davidp1404 <58428863+davidp1404@users.noreply.github.com>
2022-02-20 09:15:10 +0100

    Update enable-token-auth.sh

--- rbac/enable-token-auth.sh
+++ rbac/enable-token-auth.sh
@@ -5,3 +5,7 @@ tkdave,dave,1000,"dev"
 tkbob,bob,1001,"dba"
 EOF
 sed -i '/image:.*/i \ \ \ \ - --token-auth-file=/etc/kubernetes/pki/token-file.txt' /etc/kubernetes/manifests/kube-apiserver.yaml
+kubectl config set-credentials bob --token=tkbob
+kubectl config set-credentials dave --token=tkdave
+kubectl config set-context bob --user=bob --cluster=kubernetes
+kubectl config set-context dave --user=dave --cluster=kubernetes
